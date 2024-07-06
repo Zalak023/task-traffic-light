6c9bb58664c0cf5720a0cb412fe3c0867406a607
Zalak <157958690+Zalak023@users.noreply.github.com>
2024-07-06 17:10:06 +0530

    uplode mysql database

---
+++ signals.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 06, 2024 at 01:38 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `signal_lights`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signals`
+--
+
+CREATE TABLE `signals` (
+  `id` int(10) NOT NULL,
+  `sequence` varchar(255) NOT NULL,
+  `greenInterval` int(10) NOT NULL,
+  `yellowInterval` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `signals`
+--
+
+INSERT INTO `signals` (`id`, `sequence`, `greenInterval`, `yellowInterval`) VALUES
+(1, 'Array', 1000, 3000),
+(2, 'Array', 3000, 1000),
+(3, 'Array', 5000, 2000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `signals`
+--
+ALTER TABLE `signals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `signals`
+--
+ALTER TABLE `signals`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
